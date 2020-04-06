6a133c234237c3fee824672903c6bc27c01bafed
QurTu <martis55@inbox.lt>
2020-04-06 19:51:30 +0300

    API veikia

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ if(navigator.geolocation){
         console.log(position)
         lon = position.coords.longitude;
         lat = position.coords.latitude;
-        const API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={f5bfb48c799acb18734e01392cc425d7}`;
+        const key = "f5bfb48c799acb18734e01392cc425d7"
+        const API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
+        
         fetch(API) 
         .then(Response => {
             return Response.json();
         })
         .then(data => {
-
+                console.log(data);
         })
 
         
