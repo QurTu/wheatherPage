c0fa1b1921296cc846b754d03cbf60727317ed33
QurTu <martis55@inbox.lt>
2020-04-07 17:56:16 +0300

    use of API

--- main.js
+++ main.js
@@ -16,6 +16,28 @@ if(navigator.geolocation){
         })
         .then(data => {
                 console.log(data);
+                let n = 1 ;
+               document.querySelector(".time-zone").innerHTML = data.name;
+               document.querySelector(".temp-deg"). innerHTML = toCels(data.main.temp) + "C";
+               document.querySelector(".feels-temp").innerHTML = "jauciama temp: " + toCels(data.main.feels_like) + " C";
+               document.querySelector(".temp-deg").addEventListener("click", function(){
+                   
+    
+              
+                if( n = 0) {
+                    document.querySelector(".temp-deg"). innerHTML = toCels(data.main.temp) + "C";
+               document.querySelector(".feels-temp").innerHTML = "jauciama temp: " + toCels(data.main.feels_like) + " C";
+              n++;
+              console.log(n);
+            }
+            if (n = 1) { document.querySelector(".temp-deg"). innerHTML = toFarh(data.main.temp) + "F";
+                   document.querySelector(".feels-temp").innerHTML = "jauciama temp: " + toFarh(data.main.feels_like) + " F";
+                    n--;
+                   console.log(n);
+        }
+
+            })
+                
         })
 
         
@@ -23,4 +45,12 @@ if(navigator.geolocation){
     })
 }
 });
+
+function toCels(kelvins) {
+    return kelvins - 273,15;
+}
+function toFarh(kelvins) {
+    return kelvins * 9 / 5 -459,67;
+}
+
 
